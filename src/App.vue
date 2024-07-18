69a6b7b0c88d4bf12369a84086fb891aa271794c
Mftr96 <matteo.federici96@gmail.com>
2024-07-18 15:48:28 +0200

    milestone 5(dati stampati in pagina tramite chiamata axios al progetto laravel)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 //importo axios 
 import axios from 'axios';
+;
 export default{
   //qui metto i dati che verranno poi usati sia nei metodi sia in pagina con i binding 
   data(){
@@ -8,6 +9,7 @@ export default{
     return{
       base_link:"http://127.0.0.1:8000/",
       progetti:"",
+      link_axios:"http://127.0.0.1:8000/api/projects",
 
     }
   },
@@ -18,9 +20,9 @@ export default{
   //nel mounted inserisco le funzioni che verranno caricate all'avvio della pagina
   mounted(){
     //chiamata axios che va a recuperare i dati dei progetti
-    axios.get(`http://127.0.0.1:8000/api/projects`)
+    axios.get(this.base_link+"api/projects")
     .then((response)=>{
-      this.progetti=response.data.projects;
+      this.progetti=response.data.projects.data;
       console.log(this.progetti)
     });
   }
@@ -31,27 +33,23 @@ export default{
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1 class="text-center">ciao!ecco la lista dei progetti!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col" v-for="project in progetti">
+        <div class="card">
+          <p>{{ project.name }}</p>
+          <p>{{ project.description }}</p>
+        </div>
+   
+
+      </div>
+    </div>
   </div>
+ 
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
 </style>
