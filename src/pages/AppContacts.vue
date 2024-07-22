e302046e5e82433d1118307b6f43c853a3872c1c
Mftr96 <matteo.federici96@gmail.com>
2024-07-22 17:06:05 +0200

    creazione impalcatura di AppContacts

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -21,6 +21,42 @@ export default{
 
 <template>
        <h2>qui sei in contacts</h2>
+       <form action="">
+        <div class="mb-3">
+            <label for="" class="form-label">Name</label>
+            <input
+                type="text"
+                class="form-control"
+                name=""
+                id=""
+                aria-describedby="helpId"
+                placeholder="inserisci il tuo nome "
+            />
+            <small id="helpId" class="form-text text-muted">Help text</small>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                name=""
+                id=""
+                aria-describedby="emailHelpId"
+                placeholder="abc@mail.com"
+            />
+            <small id="emailHelpId" class="form-text text-muted"
+                >Help text</small
+            >
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">oggetto della mail</label>
+            <textarea class="form-control" name="" id="" rows="2"></textarea>
+        </div>
+        
+        
+        
+
+       </form>
 </template>
 
 <style scoped>
